540210788949fa5f56fc3f404bb111263986158f
jstoobysmith <72603918+jstoobysmith@users.noreply.github.com>
2025-06-16 13:45:52 +0000

    feat(Strings): Edit Yukawa regeneration

--- PhysLean/Particles/SuperSymmetry/SU5/Charges/Yukawa.lean
+++ PhysLean/Particles/SuperSymmetry/SU5/Charges/Yukawa.lean
@@ -26,7 +26,7 @@ open PotentialTerm
   Correspondingly, the (negative) of the charges of the singlets needed to regenerate all
   Yukawa terms in the potential. -/
 def ofYukawaTerms (x : Charges) : Multiset ℤ :=
-  x.ofPotentialTerm topYukawa + x.ofPotentialTerm bottomYukawa
+   x.ofPotentialTerm topYukawa + x.ofPotentialTerm bottomYukawa
 
 lemma ofYukawaTerms_subset_of_subset {x y : Charges} (h : x ⊆ y) :
     x.ofYukawaTerms ⊆ y.ofYukawaTerms := by
@@ -37,10 +37,10 @@ lemma ofYukawaTerms_subset_of_subset {x y : Charges} (h : x ⊆ y) :
   intro hr
   rcases hr with hr | hr
   · left
-    apply ofPotentialTerm_mono h
+    apply ofPotentialTerm_subset_of_subset h
     exact hr
   · right
-    apply ofPotentialTerm_mono h
+    apply ofPotentialTerm_subset_of_subset h
     exact hr
 
 /-- The charges of those terms which can be regenerated with up-to `n`
@@ -98,7 +98,7 @@ lemma yukawaGeneratesDangerousAtLevel_of_subset {x y : Charges} {n : ℕ} (h : x
     trans (x.ofYukawaTermsNSum n).toFinset ∩ y.phenoConstrainingChargesSP.toFinset
     · apply Finset.inter_subset_inter_left
       simp only [Multiset.toFinset_subset]
-      exact phenoConstrainingChargesSP_mono h
+      exact phenoConstrainingChargesSP_of_subset h
     · apply Finset.inter_subset_inter_right
       simp only [Multiset.toFinset_subset]
       exact ofYukawaTermsNSum_subset_of_subset h n
@@ -129,8 +129,9 @@ lemma yukawaGeneratesDangerousAtLevel_add_of_left {x : Charges} {n k : ℕ}
 lemma yukawaGeneratesDangerousAtLevel_of_le {x : Charges} {n m : ℕ}
     (h : n ≤ m) (hx : x.YukawaGeneratesDangerousAtLevel n) :
     x.YukawaGeneratesDangerousAtLevel m := by
-  generalize hk : m - n = k at *
+  let k := m - n
   have h1 : n + k = m := by omega
+  generalize k = k' at *
   subst h1
   exact yukawaGeneratesDangerousAtLevel_add_of_left hx
 
