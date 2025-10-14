f24f339ca311345c3c556e485433c21825c685f4
SophieKugelmann <sophie.kugelmann@stud.uni-greifswald.de>
2025-10-14 21:31:35 +0200

    sceleton of proof

--- PhysLean/ClassicalMechanics/ChargedParticle/Basic.lean
+++ PhysLean/ClassicalMechanics/ChargedParticle/Basic.lean
@@ -2,6 +2,9 @@ import PhysLean.Electromagnetism.Basic
 import Mathlib.LinearAlgebra.CrossProduct
 import Mathlib.Analysis.Calculus.Deriv.Basic
 import Mathlib.Analysis.InnerProductSpace.Basic
+import Mathlib.Logic.Function.Basic
+import Mathlib.Tactic.FinCases
+
 
 def path := Time → Space
 
@@ -10,7 +13,7 @@ noncomputable def velocity (r : path) : Time → Space := deriv r
 noncomputable def acceleration (r : path) : Time → Space := fun t => deriv (deriv r) t
 
 
-/-- The charged particle is specified by a mass `m`, a charge `q` and its path.
+/-- The charged particle is specified by a mass `m`, a charge `q`.
   The mass is assumed to be positive. -/
 structure ChargedParticle where
   /-- The charge of the particle -/
@@ -38,48 +41,33 @@ structure ChargedParticleInEM where
   p : ChargedParticle
 
 
-/-- The equation of motion for a charged particle in an Electromagnetic-field. -/
+/-- The equation of motion for a charged particle in an electromagnetic-field. -/
 def EquationOfMotion (EM : ChargedParticleInEM) (r : path) : Prop := ∀t ,
 EM.p.m • (acceleration r t) = EM.p.q • ((EM.E t (r t)) + ((1/c) • cross (velocity r t) (EM.B t (r t))))
 
 
 /-- The initial conditions for the motion of a particle in EM-field
-  specified by a charged particle, an electric field, a magnetic field,
-  a starting point and a given initial velocity. -/
+  specified by a starting point and a given initial velocity. -/
 structure InitialConditions where
   /-- The initial velocity. -/
   v₀ : Fin 3 → ℝ
   /-- The starting point. -/
   r₀ : Fin 3 → ℝ
 
-/-- The conditions that the particle is restricted to a plane with a perpendicular magnetic field and E=0. -/
+/-- The conditions that B is parallel to the z-axis and E=0. -/
 structure ChargedParticleInEM_restriction extends ChargedParticleInEM where
   b : ℝ
-  B_perpendicular : ∀ x : Space , ∀ t, B t x = ![0,0,b]
+  B_parallel : ∀ x : Space , ∀ t, B t x = ![0,0,b]
   E_zero: E = 0
-
-
-def plane_restriction (IC : InitialConditions) (EMS : ChargedParticleInEM) : Prop := ∀t : Time, ∀x : Space, inner ℝ (EMS.B t x) IC.v₀=0
 
 namespace ChargedParticleInEM_restriction
 
-/-- Given the initial conditions and certain restrictions named EM_Special, the solution to the equation of motion. -/
+/-- Given the ChargedParticleInEM_restrcition and initial conditions, the solution to the equation of motion. -/
 noncomputable def sol (EMS : ChargedParticleInEM_restriction)(IC: InitialConditions) : path := fun t =>
   let d := EMS.p.m*c/(EMS.p.q*EMS.b)
   ![d*(IC.v₀ 0)*(sin d⁻¹*t)-d*(IC.v₀ 1)*(cos d⁻¹*t)+(IC.r₀ 0)+d*(IC.v₀ 1),
     d*(IC.v₀ 0)*(cos d⁻¹*t)+d*(IC.v₀ 1)*(sin d⁻¹*t)+(IC.r₀ 1)+d*(IC.v₀ 0),
     IC.r₀ 2]
-
-
-lemma cross_simp (EMS : ChargedParticleInEM_restriction) (r : path) : ∀ t,
-cross (velocity r t) (EMS.B t (r t)) = EMS.b •![velocity r t 1, (-1) * (velocity r t 0), 0] := by
-  simp [cross]
-  intro t
-  have hB : EMS.B  t (r t) = ![0,0,EMS.b] := by
-    apply EMS.B_perpendicular
-  rw [hB, crossProduct]
-  simp
-  rw [mul_comm (velocity r t 1) EMS.b, mul_comm EMS.b (velocity r t 0)]
 
 
 lemma EquationOfMotion_simp (EMS : ChargedParticleInEM_restriction) (r : path) :
@@ -97,27 +85,38 @@ EMS.p.q • (1/c) • EMS.b •![velocity r t 1, (-1) * (velocity r t 0), 0] ) :
     simp
     exact h
 
-#check fderiv
+lemma comp_0 (EMS : ChargedParticleInEM_restriction)(IC : InitialConditions) (t : Time) :
+(EMS.p.m * deriv (deriv (EMS.sol IC)) t 0 = EMS.p.q * (1 / c) • (EMS.b * deriv (EMS.sol IC) t 1)):= by sorry
 
-lemma solution_true (EMS : ChargedParticleInEM_restriction) (IC : InitialConditions) : (EquationOfMotion EMS.toChargedParticleInEM (sol EMS IC)):= by sorry
+lemma comp_1 (EMS : ChargedParticleInEM_restriction)(IC : InitialConditions) (t : Time) :
+(EMS.p.m * deriv (deriv (EMS.sol IC)) t 1 = EMS.p.q * (1 / c) • (EMS.b * (-1 * deriv (EMS.sol IC) t 0))):= sorry
+
+lemma comp_2 (EMS : ChargedParticleInEM_restriction)(IC : InitialConditions) (t : Time) :
+(EMS.p.m * deriv (deriv (EMS.sol IC)) t 2 = EMS.p.q * (1 / c) • (EMS.b * 0)):= sorry
+
+lemma solution_true (EMS : ChargedParticleInEM_restriction) (IC : InitialConditions) : (EquationOfMotion EMS.toChargedParticleInEM (sol EMS IC)):= by
+  rw [EquationOfMotion_simp EMS (sol EMS IC)]
+  intro t
+  apply funext
+  intro i
+  fin_cases i
+  · dsimp [acceleration, velocity]
+    apply comp_0 EMS IC
+  · dsimp [acceleration, velocity, sol]
+    apply comp_1 EMS IC
+  · dsimp [acceleration, velocity, sol]
+    apply comp_2 EMS IC
+
+-- lemma satisfies_IC (EMS : ChargedParticleInEM_restriction) (IC : InitialConditions) : (EMS.sol IC 0 = IC.r₀ ∧ deriv EMS.sol IC 0 = IC.v₀):= by sorry
 
 
 -- The condition that there is no field and its solution.
-
 variable (p : ChargedParticle)
 variable (r₀ : Fin 3 → ℝ)
 variable (v₀ : Fin 3 → ℝ)
 
 /-- The condition that there is no field. -/
 def zeroEM : ChargedParticleInEM := ⟨0,0,p⟩
-
--- /-- The zero initial condition has zero electric field. -/
--- @[simp]
--- lemma E_zeroIC : zeroEM.E = 0 := rfl
-
--- /-- The zero initial condition has zero magnetic field. -/
--- @[simp]
--- lemma B_zeroIC : (zeroIC p v₀ r₀).B = 0 := rfl
-
+-- ...
 
 end ChargedParticleInEM_restriction
